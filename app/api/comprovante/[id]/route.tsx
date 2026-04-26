34918b605af457684949042b885f77f344abe171
YagoGuglia <yagoguglia@gmail.com>
2026-04-26 19:35:26 -0300

    fix: set api/comprovante route to force-dynamic to fix vercel build

--- app/api/comprovante/[id]/route.tsx
+++ app/api/comprovante/[id]/route.tsx
@@ -2,6 +2,8 @@ import { ImageResponse } from 'next/og';
 import { prisma } from '@/lib/prisma';
 import { NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
